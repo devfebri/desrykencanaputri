8784764273b2f54735aafc0788487af8f96a3c4b
FebriIT <bisnisfebrii@gmail.com>
2024-09-19 16:21:10 +0700

    data baru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,13 +53,14 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <title>Permata Jalan</title>
+    <title>Dinas Perhubungan Kota Jambi</title>
+
 
     <meta content="Admin Dashboard" name="description" />
     <meta content="Mannatthemes" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-    <link rel="shortcut icon" href="{{ asset('image/logo-png.png') }}">
+    <link rel="shortcut icon" href="{{ asset('img/icon.png') }}">
 
     <link href="{{ asset('template/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('template/assets/css/icons.css') }}" rel="stylesheet" type="text/css">
